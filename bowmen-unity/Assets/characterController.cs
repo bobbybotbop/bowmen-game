d1bdcc88027f6448e9a6c7bc146019a26aa3d5d4
William Chen <76022647+bobbybotbop@users.noreply.github.com>
2022-07-03 15:47:52 -0400

    a

--- bowmen-unity/Assets/characterController.cs
+++ bowmen-unity/Assets/characterController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
+
+    public FixedJoystick LeftJoystick;
+    public
+
     // Start is called before the first frame update
     void Start()
     {
